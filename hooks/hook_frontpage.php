db7b363273e2d4861c22fd9e5412743362d06ae1
Tim van Dijen <tvdijen@gmail.com>
2019-05-28 14:19:05 +0200

    Use Webmozart for assertions

--- hooks/hook_frontpage.php
+++ hooks/hook_frontpage.php
@@ -1,5 +1,7 @@
 <?php
 
+use Webmozart\Assert\Assert;
+
 /**
  * Hook to add the modinfo module to the frontpage.
  *
@@ -8,8 +10,8 @@
  */
 function sanitycheck_hook_frontpage(&$links)
 {
-    assert(is_array($links));
-    assert(array_key_exists('links', $links));
+    Assert::isArray($links);
+    Assert::keyExists($links, 'links', $links);
 
     $links['config']['sanitycheck'] = [
         'href' => SimpleSAML\Module::getModuleURL('sanitycheck/index.php'),
